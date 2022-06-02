8878483f46766cbf7db612b3bc3a46488e228365
Baharudin78 <baharudinmaulana78@gmail.com>
2022-06-02 20:59:54 +0700

    Initial Commit

---
+++ MyWebview/MyWebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWebviewApp.swift
+//  MyWebview
+//
+//  Created by Kiiroe Senkuo on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MyWebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
